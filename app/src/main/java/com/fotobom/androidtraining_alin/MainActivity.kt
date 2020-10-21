1a63f92b32fbd062607e457842575eeb8f4745f0
Alin Babu <alin@qburst.com>
2020-10-21 15:59:10 +0530

    Toast message example activity implementation

--- app/src/main/java/com/fotobom/androidtraining_alin/MainActivity.kt
+++ app/src/main/java/com/fotobom/androidtraining_alin/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.fotobom.androidtraining_alin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.cilckhere_button).setOnClickListener{
+            val intent = Intent(this@MainActivity, SubActivity::class.java)
+            startActivity(intent)
+        }
     }
+
 }
